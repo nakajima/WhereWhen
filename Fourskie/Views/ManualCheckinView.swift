0fe39f234d12a2d30dae64f11d7a343d6e45a5a4
Pat Nakajima <patnakajima@gmail.com>
2024-06-07 07:39:17 -0700

    hi

--- Fourskie/Views/ManualCheckinView.swift
+++ Fourskie/Views/ManualCheckinView.swift
@@ -16,50 +16,51 @@ struct ManualCheckinView: View {
 	@Environment(LocationListener.self) var location
 
 	@State private var status: Status = .loading
+	@State private var path: [Route] = []
 
 	enum Status {
 		case loading, loaded(CLLocation), error(String)
 	}
 
 	var body: some View {
-		NavigationStack {
-			switch status {
-			case .loading:
-				ProgressView("Finding Your Location…")
-					.task {
-						if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
-							let coord = CLLocationCoordinate2D(latitude: 37.33233141, longitude: -122.03121860)
-							let clLocation = CLLocation(coordinate: coord, altitude: 0, horizontalAccuracy: kCLLocationAccuracyNearestTenMeters, verticalAccuracy: kCLLocationAccuracyNearestTenMeters, timestamp: Date())
-							self.status = .loaded(clLocation)
-							return
-						}
-
-						do {
-							let currentLocation = try await location.requestCurrent()
-							self.status = .loaded(currentLocation)
-						} catch {
-							self.status = .error(error.localizedDescription)
-						}
+		switch status {
+		case .loading:
+			ProgressView("Finding Your Location…")
+				.task {
+					if ProcessInfo.processInfo.environment["XCODE_RUNNING_FOR_PREVIEWS"] == "1" {
+						let coord = CLLocationCoordinate2D(latitude: 37.33233141, longitude: -122.03121860)
+						let clLocation = CLLocation(coordinate: coord, altitude: 0, horizontalAccuracy: kCLLocationAccuracyNearestTenMeters, verticalAccuracy: kCLLocationAccuracyNearestTenMeters, timestamp: Date())
+						self.status = .loaded(clLocation)
+						return
 					}
-			case let .loaded(clLocation):
+
+					do {
+						let currentLocation = try await location.requestCurrent()
+						self.status = .loaded(currentLocation)
+					} catch {
+						self.status = .error(error.localizedDescription)
+					}
+				}
+		case let .loaded(clLocation):
+			NavigationContainer(path: $path) {
 				ManualCheckinChoosePlaceView(
 					location: .init(clLocation.coordinate),
 					destination: { place in .finishCheckinView(place, .init(clLocation.coordinate)) }
 				)
 				.navigationTitle("Check In")
 				.navigationBarTitleDisplayMode(.inline)
-			case let .error(string):
-				Text("Error fetching location: \(string)")
 			}
+		case let .error(string):
+			Text("Error fetching location: \(string)")
 		}
 	}
 }
 
 #if DEBUG
-	#Preview {
-		ManualCheckinView()
-			.environment(LocationListener(container: ModelContainer.preview))
-			.environmentObject(FourskieCoordinator(container: ModelContainer.preview))
-			.modelContainer(ModelContainer.preview)
-	}
+#Preview {
+	ManualCheckinView()
+		.environment(LocationListener(container: ModelContainer.preview))
+		.environmentObject(FourskieCoordinator(container: ModelContainer.preview))
+		.modelContainer(ModelContainer.preview)
+}
 #endif
---
+++ FourskieServer/Sources/LibFourskie/DeletedRecord.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Pat Nakajima on 6/7/24.
+//
+
+import Foundation
+
+public struct DeletedRecord: Codable, Sendable, Hashable {
+	public var uuid: String
+	public var type: String
+	public var deletedAt: Date
+
+	public init(uuid: String, type: String, deletedAt: Date) {
+		self.uuid = uuid
+		self.type = type
+		self.deletedAt = deletedAt
+	}
+}
