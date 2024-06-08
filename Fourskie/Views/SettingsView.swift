cc886e2d79833c6610f0b97592d0637c0f10d8ee
Pat Nakajima <patnakajima@gmail.com>
2024-06-08 15:18:53 -0700

    Add option to export database

--- Fourskie/Views/SettingsView.swift
+++ Fourskie/Views/SettingsView.swift
@@ -9,10 +9,12 @@ import Foundation
 import SwiftUI
 
 struct SettingsView: View {
+	@Environment(\.database) var database
 	@Environment(LocationListener.self) var location
 
 	var body: some View {
 		List {
+			ShareLink(item: database.url) { Text("Export Database") }
 			NavigationLink("Logs", destination: DiskLoggerViewer(logger: location.logger))
 		}
 	}
