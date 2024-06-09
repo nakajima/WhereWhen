77f38c75e53715c8f702f9faee021557f1fa2fc1
Pat Nakajima <patnakajima@gmail.com>
2024-06-08 19:55:47 -0700

    maybe fix db export

--- Fourskie/Views/SettingsView.swift
+++ Fourskie/Views/SettingsView.swift
@@ -8,18 +8,46 @@
 import Foundation
 import SwiftUI
 
+extension URL: Identifiable {
+	public var id: URL { self }
+}
+
 struct SettingsView: View {
 	@Environment(\.database) var database
 	@Environment(LocationListener.self) var location
 
+	@State private var exportURL: URL?
+
 	var body: some View {
 		List {
-			ShareLink(item: database.url) { Text("Export Database") }
+			Button(action: exportDatabase) {
+				Text("Export Database")
+			}
+			.sheet(item: $exportURL) { url in
+				ShareLink(item: url)
+			}
+
 			NavigationLink("Logs", destination: DiskLoggerViewer(logger: location.logger))
 
 			Section {
 				NavigationLink("Ignored Places", destination: IgnoredPlacesListView())
 			}
+		}
+	}
+
+	func exportDatabase() {
+		var error: NSError?
+		let coordinator = NSFileCoordinator()
+		coordinator.coordinate(readingItemAt: URL.documentsDirectory, options: [.forUploading], error: &error) { zipUrl in
+			let tmpUrl = try! FileManager.default.url(
+				for: .itemReplacementDirectory,
+				in: .userDomainMask,
+				appropriateFor: zipUrl,
+				create: true
+			).appendingPathComponent("\(database.url.lastPathComponent).zip")
+
+			try! FileManager.default.moveItem(at: zipUrl, to: tmpUrl)
+			self.exportURL = tmpUrl
 		}
 	}
 }
